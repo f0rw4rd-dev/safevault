ba757c5d5dbbd05c0aca89aa263b141e7971ed4c
f0rw4rd-dev <52598385+f0rw4rd-dev@users.noreply.github.com>
2024-03-21 10:04:36 +0300

    Update .gitignore

--- safevault/settings.py
+++ safevault/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', default=False))
+# DEBUG = bool(os.environ.get('DJANGO_DEBUG', default=False))
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ' ').split(' ')
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ' ').split(' ')
+ALLOWED_HOSTS = []
 
 # Application definition
 
